3d6018c4f8a8460273b36c6c9b2c2567defbfb9c
mati330 <matiasandres330@gmail.com>
2022-12-20 22:04:54 -0300

    se config base de datos postgrest

--- myblog/settings.py
+++ myblog/settings.py
@@ -80,10 +80,11 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
         'USER': 'root',
         'PASSWORD': 'SJPaIMhP5GG2z6GdSB7O',
         'HOST': 'containers-us-west-170.railway.app',
