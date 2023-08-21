8383fbdacddc34cf97bc4ed9083f7114a517939c
mati330 <matiasandres330@gmail.com>
2023-08-21 00:28:55 -0300

    nueva db supabase04

--- myblog/settings.py
+++ myblog/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'fgqoPP5fJxoR7Ps8',
+        'HOST': 'db.axshwmvsyqvykoavijho.supabase.co',
+        'PORT': '5432',
     }
 }
 
